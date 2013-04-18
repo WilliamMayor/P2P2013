458d5e4bba7434afec7f33d7f3deb9d4d71e401e
William Mayor <mail@williammayor.co.uk>
2013-04-18 18:08:41 +0100

    Better model using maximum likelihood

--- matlab/pePto.m
+++ matlab/pePto.m
@@ -2,18 +2,20 @@
 % i.e. this many torrents were observed to have this many peers
 raw = importdata('../data/pePto.txt', ',',1);
 % #peers,frequency 
-sorted = flipdim(sortrows(raw.data,2),1);
+sorted = flipdim(sortrows(raw.data,1),1);
 sorted = sorted(end:-1:1,:);
 
-% find line of best fit: (10^intercept.*x.^slope);
-[slope intercept r2 s] = logfit(sorted(:,1),sorted(:,2),0,'loglog');
-model=@(x) (10^intercept)*x.^(slope);
+a = 1+size(sorted,1)/sum(log(sorted(:,2)));
+model=@(x) (a-1)*x.^(-a);
 
 
 peers = sorted(:,1)';
 frequency = sorted(:,2)';
+clear idx;
 idx(cumsum([1 frequency(frequency>0)])) = 1;
 expanded = peers(cumsum(idx(1:find(idx,1,'last')-1)));
 
 mean_peers = mean(expanded);
 median_peers = median(expanded);
+
+%loglog(sorted(:,1), sorted(:,2)/sum(sorted(:,2)), sorted(:,1), model(sorted(:,1)))
